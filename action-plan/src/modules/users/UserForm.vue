312499940e43c507c32fe992dfeb41a47f47131f
Rodrigo Mendes <mendes.rodrigo@gmail.com>
2018-06-03 18:08:34 -0300

    adding field's control

--- action-plan/src/modules/users/UserForm.vue
+++ action-plan/src/modules/users/UserForm.vue
@@ -2,42 +2,59 @@
     <div class="box">
         <div class="field">
             <label class="label">Name</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Input name" v-model="name" required="true">
+            <div class="control has-icons-right">
+                <input class="input" :class="{'is-danger': showWarningIconName}" required 
+                    type="text" placeholder="Input name" v-model="name" @blur="evaluateField('name', $event)">
+                <span class="icon is-small is-right" v-show="showOkIconName" >
+                    <i class="fas fa-check"></i>
+                </span>
+                <span class="icon is-small is-right" v-show="showWarningIconName" >
+                    <i class="fas fa-exclamation-triangle"></i>
+                </span>
             </div>
+            <p class="help is-danger">{{nameFieldInfo}}</p>
         </div>
 
         <div class="field">
             <label class="label">Username</label>
             <div class="control has-icons-left has-icons-right">
-                <input class="input is-success" type="text" placeholder="Input username" required="true" v-model="username">
+                <input class="input" :class="{'is-danger': showWarningIconUsername}" type="text" placeholder="Input username" 
+                    required v-model="username" @blur="evaluateField('username', $event)">
                 <span class="icon is-small is-left">
                     <i class="fas fa-user"></i>
                 </span>
-                <span class="icon is-small is-right">
+                <span class="icon is-small is-right" v-show="showOkIconUsername" >
                     <i class="fas fa-check"></i>
                 </span>
+                <span class="icon is-small is-right" v-show="showWarningIconUsername" >
+                    <i class="fas fa-exclamation-triangle"></i>
+                </span>
             </div>
+            <p class="help is-danger">{{usernameFieldInfo}}</p>
             <p class="help is-success">This username is available</p>
         </div>
 
         <div class="field">
             <label class="label">Email</label>
             <div class="control has-icons-left has-icons-right">
-                <input class="input is-danger" type="email" placeholder="Input e-mail" required="true" v-model="email">
+                <input class="input" :class="{'is-danger': showWarningIconEmail}" type="email" 
+                    required placeholder="Input e-mail" v-model="email" @keyup="evaluateField('email', $event)">
                 <span class="icon is-small is-left">
                     <i class="fas fa-envelope"></i>
                 </span>
-                <span class="icon is-small is-right">
+                <span class="icon is-small is-right" v-show="showOkIconEmail" >
+                    <i class="fas fa-check"></i>
+                </span>
+                <span class="icon is-small is-right" v-show="showWarningIconEmail" >
                     <i class="fas fa-exclamation-triangle"></i>
                 </span>
             </div>
-            <p class="help is-danger">This email is invalid</p>
+            <p class="help is-danger">{{emailFieldInfo}}</p>
         </div>
         <div class="field">
             <div class="label">Contact phone number</div>
             <div class="control has-icons-left has-icons-right">
-                <input class="input is-danger" type="tel" placeholder="+55 61 9 9999-9999" required="false" v-model="contact">
+                <input class="input is-danger" type="tel" placeholder="+55 61 9 9999-9999" v-model="contact">
                 <span class="icon is-small is-left">
                     <i class="fas fa-phone-square"></i>
                 </span>
@@ -87,7 +104,10 @@ export default {
       name: "",
       username: "",
       email: "",
-      contact: ""
+      contact: "",
+      nameFieldInfo: "",
+      usernameFieldInfo: "",
+      emailFieldInfo: ""
     };
   },
   mounted() {
@@ -95,6 +115,11 @@ export default {
   },
   methods: {
     save() {
+      if (name.trim().length < 1) {
+        this.nameFieldInfo = "Field name is mandatory.";
+        return;
+      }
+
       let user = {
         name: this.name,
         username: this.username,
@@ -102,11 +127,62 @@ export default {
         contact: this.contact
       };
       this.$store.dispatch("saveUser", user);
+    },
+    evaluateField(field, event) {
+      let target = event.target;
+      switch (field) {
+        case "name":
+          if (!target.checkValidity()) {
+            this.nameFieldInfo = target.validationMessage;
+          } else {
+            this.nameFieldInfo = "";
+          }
+          break;
+        case "username":
+          if (!target.checkValidity()) {
+            this.usernameFieldInfo = target.validationMessage;
+          } else {
+            this.usernameFieldInfo = "";
+          }
+          break;
+        case "email":
+          if (!target.checkValidity()) {
+            this.emailFieldInfo = target.validationMessage;
+          } else {
+            this.emailFieldInfo = "";
+          }
+          break;
+      }
     }
   },
   computed: {
     users() {
       return this.$store.getters.users;
+    },
+    showWarningIconEmail() {
+      return this.emailFieldInfo.trim().length > 0;
+    },
+    showOkIconEmail() {
+      return (
+        this.emailFieldInfo.trim().length < 1 && this.email.trim().length > 0
+      );
+    },
+    showWarningIconName() {
+      return this.nameFieldInfo.trim().length > 0;
+    },
+    showOkIconName() {
+      return (
+        this.nameFieldInfo.trim().length < 1 && this.name.trim().length > 0
+      );
+    },
+    showWarningIconUsername() {
+      return this.usernameFieldInfo.trim().length > 0;
+    },
+    showOkIconUsername() {
+      return (
+        this.usernameFieldInfo.trim().length < 1 &&
+        this.username.trim().length > 0
+      );
     }
   }
 };
